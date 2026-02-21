1e5ba4a123a9d710526c86d20bb31d5252a990ee
DEEPAK A <deepaka.ad24@bitsathy.ac.in>
2026-02-21 10:14:09 +0530

    Time: 15 ms (30.81%) | Memory: 42.3 MB (79.37%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -9,8 +9,7 @@ class Solution {
     }
     private boolean isPrime(int n){
         if(n<2)return false;
-        if(n==2)return true;
-        for(int i=2;i<n;i++){
+        for(int i=2;i*i<=n;i++){
             if(n%i==0)return false;
         }
         return true;
