7d80f90acd0cdd464362916ecc707a0a28c57512
DEEPAK A <deepaka.ad24@bitsathy.ac.in>
2026-03-05 13:20:44 +0530

    Time: 8 ms (7.75%) | Memory: 44.4 MB (7.75%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -9,14 +9,14 @@ class Solution {
             if(c!=ch)count++;
             ch=(ch=='0'?'1':'0');
         }
-        int minCount=count;
-        count=0;
-        ch='1';
-        for(char c:s.toCharArray()){
-            if(c!=ch)count++;
-            ch=(ch=='0'?'1':'0');
-        }
-        minCount=Math.min(minCount,count);
+        // int minCount=count;
+        // count=0;
+        // ch='1';
+        // for(char c:s.toCharArray()){
+        //     if(c!=ch)count++;
+        //     ch=(ch=='0'?'1':'0');
+        // }
+        int minCount=Math.min(count,n-count);
         return minCount;
     }
 }
