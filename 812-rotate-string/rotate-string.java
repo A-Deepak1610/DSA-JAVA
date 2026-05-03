83371bbfe34ea026d49d2d7ab8c2e89f4f9515b4
DEEPAK A <deepaka.ad24@bitsathy.ac.in>
2026-05-03 11:50:26 +0530

    Time: 1 ms (23.75%) | Memory: 42.7 MB (58.69%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.equals(goal))return true;
+        StringBuilder sb=new StringBuilder(s);
+        for(int i=0;i<s.length();i++){
+            char ch=sb.charAt(0);
+            sb.deleteCharAt(0);
+            sb.append(ch);
+            if(goal.contentEquals(sb))return true;
+        }
+        return false;
+    }
+}
