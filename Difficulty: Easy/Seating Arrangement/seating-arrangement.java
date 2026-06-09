8e29bb83bb89da96cdb3a48cc6098481eba601bc
DEEPAK A <deepaka.ad24@bitsathy.ac.in>
2026-06-09 13:15:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Seating Arrangement/seating-arrangement.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canSeatAllPeople(int k, int[] seats) {
+        int n=seats.length;
+        for(int i=0;i<n-1;i++){
+            if(seats[i]==1&&seats[i+1]==1)return false;
+        }
+        for(int i=0;i<n;i++){
+            if(seats[i]==0){
+                boolean left=(i==0)||(seats[i-1]==0);
+                boolean right=(i==n-1)||(seats[i+1]==0);
+                if(left&&right){
+                    seats[i]=1;
+                    k--;
+                }
+                
+            }
+        }
+        return k<=0;
+    }
+}
