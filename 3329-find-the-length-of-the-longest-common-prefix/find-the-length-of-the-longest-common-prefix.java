88f7f053e25fdc6283e623578f4c78e4592dca8a
DEEPAK A <deepaka.ad24@bitsathy.ac.in>
2026-07-16 21:34:12 +0530

    Time: 38 ms (95.87%) | Memory: 81.2 MB (57.96%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
@@ -1,25 +1,65 @@
 class Solution {
+
+    class Node {
+        Node[] child = new Node[10];
+    }
+
+    Node root = new Node();
+
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        Set<String>set=new HashSet<>();
-        for(int n:arr1){
-            String s=String.valueOf(n);
-            StringBuilder temp = new StringBuilder();
-            for(int i=0;i<s.length();i++){
-                temp.append(s.charAt(i));
-                set.add(temp.toString());
-            }
+
+        // Insert arr1 numbers into trie
+        for (int num : arr1) {
+            insert(num);
         }
-        int maxLen=0;
-        for(int n:arr2){
-            String s=String.valueOf(n);
-            StringBuilder temp = new StringBuilder();
-            for(int i=0;i<s.length();i++){
-                temp.append(s.charAt(i));
-                if(set.contains(temp.toString())){
-                    maxLen=Math.max(maxLen,temp.length());
-                }
-            }
+
+        int ans = 0;
+
+        // Search arr2 numbers
+        for (int num : arr2) {
+            ans = Math.max(ans, search(num));
         }
-        return maxLen;
+
+        return ans;
+    }
+
+    private void insert(int num) {
+
+        Node node = root;
+
+        String s = String.valueOf(num);
+
+        for (char c : s.toCharArray()) {
+
+            int digit = c - '0';
+
+            if (node.child[digit] == null) {
+                node.child[digit] = new Node();
+            }
+
+            node = node.child[digit];
+        }
+    }
+
+
+    private int search(int num) {
+
+        Node node = root;
+        int len = 0;
+
+        String s = String.valueOf(num);
+
+        for (char c : s.toCharArray()) {
+
+            int digit = c - '0';
+
+            if (node.child[digit] == null)
+                break;
+
+            len++;
+            node = node.child[digit];
+        }
+
+        return len;
     }
 }
