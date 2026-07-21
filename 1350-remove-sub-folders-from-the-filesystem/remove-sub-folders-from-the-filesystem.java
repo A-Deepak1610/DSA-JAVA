d88f78c2618f42cb0786e52dbc4aef015372bb88
DEEPAK A <deepaka.ad24@bitsathy.ac.in>
2026-07-21 10:09:18 +0530

    Time: 57 ms (39.16%) | Memory: 74.3 MB (25.06%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.java
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.java
@@ -1,12 +1,54 @@
+class TrieNode {
+        Map<String, TrieNode> children = new HashMap<>();
+        boolean isFolder;
+    }
+
 class Solution {
+    TrieNode root = new TrieNode();
     public List<String> removeSubfolders(String[] folder) {
-        List<String>res=new ArrayList<>();
-        Arrays.sort(folder);
-        for(String s:folder){
-            if(res.isEmpty()||!s.startsWith(res.get(res.size()-1)+"/")){
-                res.add(s);
-            }
+        // Build Trie
+        for (String path : folder) {
+            insert(path);
         }
-        return res;
+        List<String> ans = new ArrayList<>();
+        // Collect only parent folders
+        dfs(root, "", ans);
+        return ans;
+    }
+
+    private void insert(String path) {
+        String[] parts = path.split("/");
+        TrieNode node = root;
+        for (int i = 1; i < parts.length; i++) {
+            // If a parent folder already exists
+            if (node.isFolder) {
+                return;
+            }
+            node.children.putIfAbsent(
+                parts[i],
+                new TrieNode()
+            );
+            node = node.children.get(parts[i]);
+        }
+        node.isFolder = true;
+    }
+
+
+    private void dfs(TrieNode node, String path, List<String> ans) {
+        // If this is a folder, add it and stop
+        if (node.isFolder) {
+            ans.add(path);
+            return;
+        }
+        for (String child : node.children.keySet()) {
+            String newPath;
+            if (path.equals("")) {
+                newPath = "/" + child;
+            } 
+            else {
+                newPath = path + "/" + child;
+            }
+            dfs(node.children.get(child), newPath, ans);
+        }
     }
 }
