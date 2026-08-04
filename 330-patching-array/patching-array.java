ed490689c996164614824a2a2cf697e1b6896471
DEEPAK A <deepaka.ad24@bitsathy.ac.in>
2026-08-04 16:02:17 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (23.67%) - LeetSync

---
+++ 330-patching-array/patching-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        long miss=1;
+        int i=0,patch=0;
+        while(miss<=n){
+            if(i<nums.length&&nums[i]<=miss)miss+=(long)nums[i++];
+            else{
+                miss+=miss;
+                patch++;
+            }
+        }
+        return patch;
+    }
+}
