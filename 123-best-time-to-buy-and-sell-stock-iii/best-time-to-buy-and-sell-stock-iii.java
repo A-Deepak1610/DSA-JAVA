cb08d6a21ff9d1dc06d9d5fdf2de8d8984e41b9b
DEEPAK A <deepaka.ad24@bitsathy.ac.in>
2026-08-13 21:46:10 +0530

    Time: 137 ms (12.64%) | Memory: 134.3 MB (19.38%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int[][][] dp=new int[prices.length][2][3];
+        for(int[][] d:dp){
+            for(int[] d1:d)Arrays.fill(d1,-1);
+        }
+        return solve(prices,0,true,2,dp);
+    }
+    private int solve(int[] prices,int idx,boolean canBuy,int transcation,int[][][] dp){
+        if(idx==prices.length||transcation==0)return 0;
+        int profit=0;
+        if(dp[idx][canBuy?0:1][transcation]!=-1)return dp[idx][canBuy?0:1][transcation];
+        if(canBuy){
+            int buy=-prices[idx]+solve(prices,idx+1,false,transcation,dp);
+            int skip=solve(prices,idx+1,canBuy,transcation,dp);
+            profit=Math.max(buy,skip);
+        }
+        else{
+            int sell=prices[idx]+solve(prices,idx+1,true,transcation-1,dp);
+            int skip=solve(prices,idx+1,canBuy,transcation,dp);
+            profit=Math.max(sell,skip);
+        }
+        return dp[idx][canBuy?0:1][transcation]=profit;
+    }
+}
